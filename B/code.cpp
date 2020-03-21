0571ce231f6f8f3e730584abd713797485f8581e
tom <tom@365en.onmicrosoft.com>
2020-03-21 13:23:17 +1100

    added A and B

---
+++ B/code.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dist2(int x1, int x2, int y1, int y2 {
+  int dx = x1-x2;
+  int dy = y1-y2;
+  return dx*dx + dy*dy;
+}
+
+int main() {
+  cin >> x >> y >> x1 >> y1 >> x2 >> y2;
+  int answer2 = 1000000000;
+  answer = min(dist(x, y, x1, y1));
+  answer = min(dist(x, y, x1, y1));
+  answer = min(dist(x, y, x1, y1));
+  answer = min(dist(x, y, x1, y1));
+  cout << sqrt((double) answer2) << endl;
+}
