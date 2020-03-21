1d2b021292687fe2aa9c3631049ed8d4ffee6130
tom <tom@365en.onmicrosoft.com>
2020-03-21 15:24:51 +1100

    init E

--- D/code.cpp
+++ D/code.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #define STATES 1679616
+#define INF STATES*2
 #define MAXN 8
 #define MAXV 6
 using namespace std;
@@ -16,27 +17,61 @@ int str2int(string& str) {
 int dist[STATES];
 vector<int> edge[STATES];
 
+void rot(string& str, int pos) {
+  str[pos] = 'A' + ((str[pos] - 'A' + 1) % MAXV);
+}
+
 void calculate_edges(int at) {
-  if (edge[at]) return;
-  int str[8];
+  if (edge[at].size()) return;
+
+  string original_str = "AAAAAAAA";
   int v = at;
   for (int i = MAXN-1; i >= 0; i--) {
-    str[i] = v % MAXV;
+    original_str[i] = (v % MAXV) + 'A';
     v = v / MAXV;
   }
 
-  for (int i = 0; i < 8; i++) {
-    if (str[i] == 'A') {
-
+  for (int i = 0; i < MAXN; i++) {
+    string str = original_str;
+    switch (original_str[i]) {
+      case 'A':
+        if (i != 0) rot(str, i-1);
+        if (i != MAXN-1) rot(str, i+1);
+        break;
+      case 'B':
+        if (i != 0 && i != MAXN-1) str[i+1] = str[i-1];
+        break;
+      case 'C':
+        rot(str, 7-i);
+        break;
+      case 'D':
+        if (i <= 3) {
+          for (int j = 0; j < i; j++) rot(str, j);
+        } else {
+          for (int j = i+1; j < MAXN; j++) rot(str, j);
+        }
+        break;
+      case 'E':
+        if (i != 0 && i != MAXN-1) {
+          if (i <= 3) {
+            int dist = i;
+            rot(str, 0);
+            rot(str, i+dist);
+          } else {
+            int dist = MAXN-1-i;
+            rot(str, MAXN-1);
+            rot(str, i-dist);
+          }
+        }
+        break;
+      case 'F':
+        if ((i+1) % 2 == 0) rot(str, ((i+1)/2)-1);
+        else rot(str, (((i+1)+9)/2)-1);
+        break;
     }
+    edge[at].push_back(str2int(str));
   }
-
-  // operation A
-  // operation B
-  // operation C
-  // operation D
-  // operation E
-  // operation F
+  // cout << edge[at].size() << endl;
 }
 
 string s;
@@ -45,4 +80,32 @@ int main() {
   cin >> s >> t;
   int sn = str2int(s);
   int tn = str2int(t);
+  fill(dist, dist+STATES, INF);
+
+  deque<int> q;
+  q.push_back(sn);
+  dist[sn] = 0;
+
+  for (int i = 0; i < STATES; i++) {
+    calculate_edges(i);
+  }
+
+  while (q.size()) {
+    int at = q.front();
+    q.pop_front();
+
+    if (at == tn) {
+      cout << dist[at] << endl;
+      return 0;
+    }
+
+    for (int next : edge[at]) {
+      if (dist[next] == INF) {
+        dist[next] = dist[at] + 1;
+        q.push_back(next);
+      }
+    }
+  }
+
+  cout << -1 << endl;
 }
